001e1a92411de1db491734087e5e06f52ba850ae
paulg92 <paul.gules@gmail.com>
2019-10-16 15:17:44 +0200

    added some more functionality you can send tweets now

--- app/src/main/java/com/example/twitterclone/SendTweetActivity.java
+++ app/src/main/java/com/example/twitterclone/SendTweetActivity.java
@@ -7,20 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SendTweetActivity extends AppCompatActivity {
 
     private EditText mSendTweet;
+    private ListView mListView;
     private Button mButtonSendTweet;
-
+    private Button mButtonViewTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,38 @@ public class SendTweetActivity extends AppCompatActivity {
 
         mSendTweet = findViewById(R.id.TweetMessage);
         mButtonSendTweet = findViewById(R.id.buttonSendTweet);
+        mListView = findViewById(R.id.viewTweetsListView);
+        mButtonViewTweets = findViewById(R.id.btnviewOtherUsersTweets);
+
+        mButtonViewTweets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ArrayList<HashMap<String, String>> tweetList = new ArrayList<>();
+                final SimpleAdapter adapter = new SimpleAdapter(SendTweetActivity.this,
+                        tweetList, android.R.layout.simple_list_item_2, new String[]{"tweetUserName",
+                        "tweetValue"}, new int[]{android.R.id.text1, android.R.id.text2});
+                try {
+                    ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("MyTweet");
+                    parseQuery.whereContainedIn("user", ParseUser.getCurrentUser().getList("fanOf"));
+                    parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                        @Override
+                        public void done(List<ParseObject> objects, ParseException e) {
+                            if (objects.size() > 0 && e == null) {
+                                for (ParseObject tweetObjects : objects) {
+                                    HashMap<String, String> userTweet = new HashMap<>();
+                                    userTweet.put("tweetUserName", tweetObjects.getString("user"));
+                                    userTweet.put("tweetValue", tweetObjects.getString("tweet"));
+                                    tweetList.add(userTweet);
+                                }
+                                mListView.setAdapter(adapter);
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         mButtonSendTweet.setOnClickListener(new View.OnClickListener() {
             @Override
